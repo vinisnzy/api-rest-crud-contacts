b1f31b3b24993833cec8b79d20188e8ebe0d9b3a
Vinícius de Paula <manoseilacara123@gmail.com>
2025-09-06 14:14:39 -0300

    feat: add phone, name and last name queries in contact repository

--- src/main/java/com/vinisnzy/api_rest_crud/repository/ContactRepository.java
+++ src/main/java/com/vinisnzy/api_rest_crud/repository/ContactRepository.java
@@ -3,5 +3,10 @@ package com.vinisnzy.api_rest_crud.repository;
 import com.vinisnzy.api_rest_crud.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+    Optional<Contact> findByPhone(String phone);
+    Optional<Contact> findByName(String name);
+    Optional<Contact> findByLastName(String lastName);
 }
