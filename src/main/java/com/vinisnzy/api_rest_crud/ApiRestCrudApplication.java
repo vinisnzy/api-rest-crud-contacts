c50687e6de6fbd4de1c3efb26efe9fa6964931c8
Vinícius de Paula <manoseilacara123@gmail.com>
2025-09-06 10:40:30 -0300

    initial commit

---
+++ src/main/java/com/vinisnzy/api_rest_crud/ApiRestCrudApplication.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.api_rest_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinisnzy/api_rest_crud/ApiRestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.api_rest_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
