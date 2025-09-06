f8056a3e36f35addf6d23af361de9140e65abedc
Vinícius de Paula <manoseilacara123@gmail.com>
2025-09-06 15:33:17 -0300

    fix: add csrf.disable() and add session stateless

--- src/main/java/com/vinisnzy/api_rest_crud/config/SecurityConfig.java
+++ src/main/java/com/vinisnzy/api_rest_crud/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.vinisnzy.api_rest_crud.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -13,11 +15,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+        http.csrf(AbstractHttpConfigurer::disable)
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll())
-                .formLogin(withDefaults());
+                .formLogin(withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
